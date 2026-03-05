0cf64b7351cf98923df9a7e62a01c07708c340c2
Tom Adams <tom.adams101@gmail.com>
2026-03-05 20:19:51 +0000

    File changes

--- src/pages/AuthRedirect.jsx
+++ src/pages/AuthRedirect.jsx
@@ -156,7 +156,7 @@ export default function AuthRedirect() {
 
       window.location.assign(subscribeUrl);
     })();
-  }, [season?.isLoading, season?.accountId, season?.hasAccess, season?.entitlement, next, nav]);
+  }, [season?.isLoading, season?.accountId, season?.hasAccess, season?.entitlement, next, nav, isCheckoutReturn]);
 
   return (
     <div className="min-h-screen bg-[#0a0e1a] flex items-center justify-center">
